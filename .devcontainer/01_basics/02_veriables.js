8127dc969df2d3896c618b8c8fe7c45ecb84d3e8
pavanbirajdar91 <114732439+pavanbirajdar91@users.noreply.github.com>
2024-12-22 18:13:42 +0000

    datatype done

---
+++ .devcontainer/01_basics/02_veriables.js
@@ -0,0 +1,30 @@
+/*
+"use strict"// if we use this key word in program then it will treat as a newwe verjoin 
+
+
+data type
+number
+bigint = big number use
+string  = ""
+boolean = true/false
+null = standAlone value it is 
+undefine = when any veriable not carring value then it will be define
+symbol = unique 
+
+object = 
+
+assiment =collect all the sepcification for datatypes
+
+
+
+
+
+*/
+
+"use strict";
+
+//alert(3+3) //we are using nodejs,not browser
+
+console.log(3+3)
+
+
