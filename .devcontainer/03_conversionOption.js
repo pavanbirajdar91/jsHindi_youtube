1783b54f32398be13b33c1444739efc81ebfa932
pavanbirajdar91 <114732439+pavanbirajdar91@users.noreply.github.com>
2024-12-23 16:55:36 +0000

    convergion of data type

---
+++ .devcontainer/03_conversionOption.js
@@ -0,0 +1,30 @@
+//let score = "pavan"
+//console.log(typeof score);
+
+//console.log(typeof (score));
+
+//let valueNumber = Number(score)
+
+//console.log(typeof valueNumber);
+
+//console.log(valueNumber);
+
+//"33" ==> 33
+//"33abc" ==> NaN
+// true ==> 1; false ==> 0
+let isLoggedIn = "pavan"
+
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+ //console.log(booleanIsLoggedIn);
+ 
+//convert string
+
+
+let someNomber = 33
+let stringNumber = String(someNomber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
